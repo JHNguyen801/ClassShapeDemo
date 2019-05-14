1662f23d32fed03f73657d6b5655e045b2db2726
Hoang Nguyen <huy_hoang28@hotmail.com>
2019-05-14 17:54:13 -0600

    Add project files.

---
+++ Assignment_6_Solution_2/Program.cs
@@ -0,0 +1,36 @@
+﻿// The program is designed to use an abstract class Shape that support
+// the ability to request the automatically implement string propety, 
+// area method, and its perimeter method. In addition, the property Name
+// has a private set implementation and public get implementation. 
+// The area and perimeter methods have to be abstract and public.
+using System;
+using System.Collections.Generic;
+
+namespace Assignment_6_Solution_2
+{
+    // This class program contains the list, putinShapeList method. When it calls 
+    // the program display the shape information on the list and its area and perimeter.
+    class Program
+    {
+        // Create an instance of the list that hold the class Shape 
+        private static List<Shape> TheShapes = new List<Shape>();
+        // Static method that add instance of shape to the list theShapes.
+        static void putInShapeList(Shape S) { TheShapes.Add(S); }
+        // This method is to place instances of a circle and square in a List of the 
+        // base class type and iterate through the List to print out the derived 
+        // instances name, perimeter and area.
+        static void Main(string[] args)
+        {
+            // Call the putInShapeList method hold the circle radius and square side.
+            putInShapeList(new circle("circle", 10.0));
+            putInShapeList(new square("square", 5));
+            // To display how many shapes are on the list
+            Console.WriteLine($"There are {TheShapes.Count} shapes in our list");
+            // Loop through the object, name, circle, and square on the list and 
+            // to display the object shape, area, and perimeter.
+            foreach (var S in TheShapes)
+                Console.WriteLine($"\tThe object is a {S.Name}, its perimeter is " +
+                    $"{S.Perimeter():F5} " + $"and the Area is {S.Area():F5}");
+        }
+    }
+}
---
+++ Assignment_6_Solution_2/Shape.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Assignment_6_Solution_2
+{
+    // Class: Shape
+    // Summary: Class shape contains the property name, a constructor holds the 
+    // information and abstract methods.
+    public abstract class Shape
+    {
+        // Name property to store the received string
+        public string Name
+        { get; private set; }
+        // Shape constructor hold the string name of the shape
+        public Shape(string name)
+        { Name = name; }
+        // Abstract method of the shape area to be override by derived class
+        public abstract double Area();
+        // Abstract method of the shape perimeter to be override by derived class
+        public abstract double Perimeter();
+    }
+    // Class: Circle
+    // Summary: Radius derived from the base class property and methods. 
+    // It has override area and perimeter method.
+    public class circle : Shape
+    {
+        // Initialized the radius of the circle
+        private double Radius;
+        // Circle constructor derived from the base N parameter
+        // that hold the radius and string name
+        public circle(string name, double radius) : base(name)
+        {
+            Radius = radius;
+        }
+        // Radius property read and write field radius 
+        public double radius { get; private set; }
+        // Override method return the area of the circle
+        public override double Area()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+        // Override Perimeter method return the perimeter of a circle
+        public override double Perimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+    }
+    // Class: Square
+    // Summary: Class square derived from the base class property and methods. 
+    // It has override area and perimethod method.
+    public class square : Shape
+    {
+        // Create a private side field of the class
+        private double Side;
+        // Square constructor derived from the base N parameter
+        // that hold the side and string name
+        public square(string name, double side) : base(name)
+        { Side = side; }
+        // Override method return the area of the square
+        public override double Area()
+        {
+            return Math.Pow(Side, 2);
+        }
+        // Override method return the perimeter of a square
+        public override double Perimeter()
+        { return 4 * Side; }
+    }
+}
